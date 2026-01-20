49437ec609f3e475f0247e663b045e7346dc3fd8
Maaz Fatima <maazfatima21@gmail.com>
2026-01-20 20:15:12 +0530

    Add auth, admin protection, and product creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,11 +12,11 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import AddProduct from "./pages/AddProduct";
 import EditProduct from "./pages/EditProduct";
-import Cart from './pages/Cart';
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
 
 import ProductProvider from "./context/ProductContext";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import Checkout from "./pages/Checkout";
 
 function App() {
   return (
@@ -25,84 +25,21 @@ function App() {
         <Navbar />
 
         <Routes>
-          {/* ---------- PUBLIC ROUTE ---------- */}
+          {/* Public */}
           <Route path="/login" element={<Login />} />
 
-          {/* ---------- PROTECTED ROUTES (Logged-in users only) ---------- */}
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <Home />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/products"
-            element={
-              <ProtectedRoute>
-                <Products />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/products/:id"
-            element={
-              <ProtectedRoute>
-                <ProductPage />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <ProtectedRoute>
-                <About />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/contact"
-            element={
-              <ProtectedRoute>
-                <Contact />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/cart"
-            element={
-              <ProtectedRoute>
-                <Cart />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/checkout"
-            element={
-              <ProtectedRoute>
-                <Checkout />
-              </ProtectedRoute>
-            }
-          />
+          {/* Protected */}
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/products" element={<ProtectedRoute><Products /></ProtectedRoute>} />
+          <Route path="/products/:id" element={<ProtectedRoute><ProductPage /></ProtectedRoute>} />
+          <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
+          <Route path="/contact" element={<ProtectedRoute><Contact /></ProtectedRoute>} />
+          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path="/checkout" element={<ProtectedRoute><Checkout /></ProtectedRoute>} />
 
-          {/* ---------- ADMIN-ONLY ROUTES ---------- */}
-          <Route
-            path="/add-product"
-            element={
-              <ProtectedRoute adminOnly={true}>
-                <AddProduct />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/edit-product/:id"
-            element={
-              <ProtectedRoute adminOnly={true}>
-                <EditProduct />
-              </ProtectedRoute>
-            }
-          />
+          {/* Admin only */}
+          <Route path="/add-product" element={<ProtectedRoute adminOnly><AddProduct /></ProtectedRoute>} />
+          <Route path="/edit-product/:id" element={<ProtectedRoute adminOnly><EditProduct /></ProtectedRoute>} />
         </Routes>
 
         <Footer />
